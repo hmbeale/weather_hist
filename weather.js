edcc35d2f89e16d79e56682c4d65148a85ce8936
Holden Beale <itdbealh@aikencounty.net>
2018-09-13 15:09:31 -0400

    parsed the JSON and displayed

--- weather.js
+++ weather.js
@@ -1,13 +1,37 @@
-const url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-01';
-const my_token = 'gsaMCPslBfZssouUOqGlLxlwSFQWMdoa';
-const otherParam = {
-  headers:{ token: my_token }
+const fetchWeather = (zip) =>{
+  const url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:' + zip + '&startdate=2015-10-04&enddate=2015-10-04&units=standard';
+  const my_token = 'gsaMCPslBfZssouUOqGlLxlwSFQWMdoa';
+  const otherParam = {
+    headers:{ token: my_token }
+  }
+
+  fetch(url, otherParam)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      const precip = [];
+      for (let i = 0; i < response.results.length; i++) {
+        if (response.results[i].datatype == 'PRCP'){
+          precip.push(response.results[i].value)
+        }
+      }
+      let avg = 0;
+      for (let i = 0; i < precip.length; i++){
+        avg+= precip[i];
+      }
+      avg = avg/precip.length;
+      displayWeather(avg);
+    });
 }
 
-fetch(url, otherParam)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+const displayWeather = (rainAvg) =>{
+  document.getElementById("display").textContent =
+  `On October 4th, 2015, there were ${Math.round(rainAvg*100)/100}
+  inches of rain there.`
+}
+
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  fetchWeather(document.querySelector("form").elements["zip"].value);
+})
